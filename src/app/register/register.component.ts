f9f5fca5b32d1933185d265dd5bbed814c555bc5
David Atanda <ololadedavid15@gmail.com>
2023-03-07 14:26:27 +0100

    now sending request

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { BackendService } from '../services/backend.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  hide = true
 
-  constructor() { }
+  constructor(public backend: BackendService) { }
+  public registerForm = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/)]),
+    orgName: new FormControl('', Validators.required)
+  })
 
   ngOnInit(): void {
+  }
+
+  register(){
+    if(this.registerForm.status == 'VALID'){
+      console.log(this.registerForm.value)
+      this.backend.register(this.registerForm.value).subscribe((res:any)=>{
+        console.log(res)
+      })
+    }else{}
   }
 
 }
