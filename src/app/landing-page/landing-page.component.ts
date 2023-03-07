cf7d36c03a63a00a2eeafa10b3280adcfd16bb6d
David Atanda <ololadedavid15@gmail.com>
2023-03-07 12:24:25 +0100

    landing page

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor(public router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  start(){
+    this.router.navigate(['/register'])
+  }
+
+}
