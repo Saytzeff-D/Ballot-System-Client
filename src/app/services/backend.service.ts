5f0cc176950d31b4b39e05743fa44b6b538bb6d2
David Atanda <ololadedavid15@gmail.com>
2023-03-07 14:25:43 +0100

    config the service

---
+++ src/app/services/backend.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+
+  constructor(public http: HttpClient) { }
+  public baseUrl = environment.baseUrl
+
+  register(value:any){
+    return this.http.post(`${this.baseUrl}account/register`, value)
+  }
+}
