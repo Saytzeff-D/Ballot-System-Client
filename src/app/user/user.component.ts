5585925d5d5399979bdbf17f61bcf596b7e5b8e4
David Atanda <ololadedavid15@gmail.com>
2023-03-09 13:52:33 +0100

    constructed a minor validation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { BehaviourService } from '../services/behaviour.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +17,22 @@ export class UserComponent {
       map(result => result.matches),
       shareReplay()
     );
+    public user:any;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public behaviorService: BehaviourService,
+    public router: Router
+    ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    if (sessionStorage.getItem('user') !== null) {
+      this.behaviorService.user.next(JSON.parse(sessionStorage.getItem('user')!))
+    } else {
+      this.router.navigate(['/login'])
+    }
+  }
 
 }
