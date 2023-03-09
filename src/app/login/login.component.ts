5894046a991952db967cb4af6b01869a2d60d16d
David Atanda <ololadedavid15@gmail.com>
2023-03-09 13:51:52 +0100

    user can now login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
 import { BackendService } from '../services/backend.service';
 
 @Component({
@@ -10,14 +11,38 @@ import { BackendService } from '../services/backend.service';
 export class LoginComponent implements OnInit {
   hide = true
 
-  constructor() { }
+  constructor(public backend: BackendService, public router: Router) { }
   public loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
   })
+  public loading = false;
+  public error:any = ''
 
   ngOnInit(): void {
   }
 
-  login(): void{}
+  login(): void{
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value)
+      this.loading = true
+      console.log(this.loginForm.value)
+      this.backend.login(this.loginForm.value).subscribe((res:any)=>{
+        console.log(res)
+        if (res.msg == 'Success') {
+          sessionStorage.setItem('user', JSON.stringify(res.data))
+          this.router.navigate(['/account/dashboard'])
+        } else {
+          this.loading = false
+          this.error = res.msg
+        }
+      }, (err:any)=>{
+        if(err){
+          this.loading = false
+          console.log(err)
+          this.error = err.statusText
+        }
+      })
+    } else { }
+  }
 }
